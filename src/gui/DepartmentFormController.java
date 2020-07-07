ff5e65faf42a09508d7c838a3231b0f09e4f1f0a
yamashm <awnmist@gmail.com>
2020-07-07 17:40:51 -0300

    Saving new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
-	
+	private DepartmentService service;
+
 	@FXML
 	private TextField txtId;
 	@FXML
@@ -25,33 +32,59 @@ public class DepartmentFormController implements Initializable {
 	private Button btSave;
 	@FXML
 	private Button btCancel;
-	
+
 	public void setDepartment(Department entity) {
 		this.entity = entity;
 	}
-	
-	@FXML
-	public void onBtSaveAction() {
-		
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
-	
+
 	@FXML
-	public void onBtCancelAction() {
-		
+	public void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity null.");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service null.");
+		}
+		try {
+
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} catch (DbException e) {
+			Alerts.showAlert("Erro ao salvar objeto", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
-	
+
+	private Department getFormData() {
+		Department obj = new Department();
+
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
 	}
-	
+
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 32);
 	}
-	
+
 	public void updateFormData() {
-		if(entity == null) {
+		if (entity == null) {
 			throw new IllegalStateException("Entity null.");
 		}
 		txtId.setText(String.valueOf(entity.getId()));
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -18,4 +18,13 @@ public class DepartmentService {
 //		return list;
 		return dao.findAll();
 	}
+	
+	public void saveOrUpdate(Department obj) {
+		if(obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
 }
