59037798ef491eca10704a7f46f9e6304f5d1734
yamashm <awnmist@gmail.com>
2020-07-07 16:29:16 -0300

    Database access

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,17 +1,21 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
+	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
 	public List<Department> findAll(){
-		List<Department> list = new ArrayList<Department>();
-		list.add(new Department(1, "Livros"));
-		list.add(new Department(2, "Computadores"));
-		list.add(new Department(3, "Eletrônicos"));
-		return list;
+//		List<Department> list = new ArrayList<Department>();
+//		list.add(new Department(1, "Livros"));
+//		list.add(new Department(2, "Computadores"));
+//		list.add(new Department(3, "Eletrônicos"));
+//		return list;
+		return dao.findAll();
 	}
 }
